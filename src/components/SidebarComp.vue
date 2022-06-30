ddaa5714f75197f32cdb7abec466545c0ad4ad65
asepsn <asepsn7@gmail.com>
2022-06-30 20:09:28 +0700

    init

---
+++ src/components/SidebarComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="col-3 px-1 position-fixed" id="sticky-sidebar">
+      <div class="nav flex-column flex-nowrap vh-100 overflow-auto text-white p-2">
+          <a class="navbar-brand link-light"  href="#"><h4>Tweetty</h4></a>
+          <a href="./" class="nav-link link-light"><h5>Home</h5></a>
+          <a href="./" class="nav-link link-light"><h5>Explore</h5></a>
+          <a href="./" class="nav-link link-light"><h5>Notification</h5></a>
+          <a href="./" class="nav-link link-light"><h5>Message</h5></a>
+          <a href="./" class="nav-link link-light"><h5>Profile</h5></a>
+          <a href="./" class="nav-link link-light"><h5>Tweetty Post</h5></a>
+      </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- header -->
+    <HeaderComp/>
+
+    <div class="row">
+      <!-- sidebar -->
+      <div class="col-md-4">
+        <SidebarComp/>
+      </div>
+      <!-- isi -->
+      <div class="col-md-4">
+asd
+      </div>
+
+      <div class="col-md-4">
+
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderComp from '@/components/HeaderComp.vue'
+import SidebarComp from '@/components/SidebarComp.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderComp,
+    SidebarComp
+  }
+}
+</script>
